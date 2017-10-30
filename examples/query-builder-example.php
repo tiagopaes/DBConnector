d6beffb35591ea1b1f00a83d3731602babe1e8e4
tiagopaes <tiagopaes37@gmail.com>
2017-10-30 11:28:12 -0200

    add query builder example file

---
+++ examples/query-builder-example.php
@@ -0,0 +1,48 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use PhpDao\Connection;
+use PhpDao\QueryBuilder;
+
+$options = [
+    'host' => 'localhost',
+    'database' => 'qa_tools',
+    'user' => 'tiago',
+    'password' => '',
+    'port' => '3306',
+    'driver' => 'mysql'
+];
+
+//Instance the Connection class and pass the database options on constructor
+$connection = new Connection($options);
+
+//Instance the QueryBuilder class and pass the connection class on constructor
+$queryBuilder = new QueryBuilder($connection);
+
+// insert example 
+$insertedId = $queryBuilder->table('users')
+    ->fields(['email', 'password'])
+    ->insert(['email@test.com', crypt('password')]);
+
+//select examples
+$tableValues = $queryBuilder->table('users')
+    ->fields(['*'])
+    ->select([]);
+
+$tableValues = $queryBuilder->table('users')
+    ->fields(['email'])
+    ->where('email = ?')
+    ->limit('1')
+    ->select(['email@test.com']);
+
+// update example
+$queryBuilder->table('users')
+    ->fields(['email'])
+    ->where('id = ?')
+    ->update(['email@updated.com'], [$insertedId]);
+
+// delete example
+$tableValues = $queryBuilder->table('users')
+    ->where('id = ?')
+    ->delete([$insertedId]);
